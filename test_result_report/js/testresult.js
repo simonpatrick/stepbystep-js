e6db7ef6fc28b7e94e89e170ad79e37edbe0efa2
patrick <patrick@patrickdeMacBook-Pro.local>
2015-11-13 14:59:28 +0800

    update test result template

--- test_result_report/js/testresult.js
+++ test_result_report/js/testresult.js
@@ -8,26 +8,31 @@ new Vue({
             {name:"Name1",cases:100,isPass:true,passed:100,failed:10,testCases:[{
                 name: "test1",
                 isPass: true,
-                logs:["testlogging1","testlog2"]
+                logs:["testlogging1","testlog2"],
+                images:["add_1.jpg"]
             }]},
             {name:"Name2",cases:100,isPass:true,passed:100,failed:10,testCases:[{
                 name: "test2",
                 isPass: true,
-                logs:["testlogging1","testlog2"]
+                logs:["testlogging1","testlog2"],
+                images:["add_1.jpg","add_2.jpg"]
             },{
                 name: "test3",
                 isPass: false,
-                logs:["testlogging1","testlog2"]
+                logs:["testlogging1","testlog2"],
+                images:["add_1.jpg"]
             }]},
             {name:"Name3",cases:100,isPass: true ,passed:100,failed:10,testCases:[{
                 name: "test3",
                 isPass: false,
-                logs:["testlogging1","testlog2"]
+                logs:["testlogging1","testlog2"],
+                images:["add_1.jpg","add_2.jpg","add_3.jpg","add_4.jpg"]
             }]},
             {name:"Name4",cases:100,isPass: true ,passed:100,failed:10,testCases:[{
                 name: "test3",
                 isPass: false,
-                logs:["testlogging1","testlog2"]
+                logs:["testlogging1","testlog2"],
+                images:["add_1.jpg","add_2.jpg","add_3.jpg","add_4.jpg"]
             }]}
         ],
     },
