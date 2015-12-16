9d173643629af2dff583a48137fd055df8bbf24b
simonpatrick <simonpatrick@163.com>
2015-12-16 18:21:30 +0800

    update threads

--- gulp_samples/vue_learning/gulpfile.js
+++ gulp_samples/vue_learning/gulpfile.js
@@ -1,31 +1,29 @@
 var gulp = require('gulp');
 var qunit = require('gulp-qunit');
-var jshint=require('gulp-jshint');
-var htmlhint=require('gulp-htmlhint');
-var csslint=require('gulp-csslint');
+var jshint = require('gulp-jshint');
+var htmlhint = require('gulp-htmlhint');
+var csslint = require('gulp-csslint');
 
-gulp.task
+gulp.task('default', function () {
 
-gulp.task('default',function(){
-  
 });
 
-gulp.task('test',function(){
+gulp.task('test', function () {
     return gulp.src('travis-stats/test.html')
-        .pipe(qunit({'phantomjs-options':['--ignore-ssh-errors=true']}));
+        .pipe(qunit({'phantomjs-options': ['--ignore-ssh-errors=true']}));
 });
 
-gulp.task('jslint',function(){
-   return gulp.src('travis-stats/*.js')
-       .pipe(jshint()).pipe(jshint().reporter('default')).pipe(jshint.reporter('fail'));
+gulp.task('jslint', function () {
+    return gulp.src('travis-stats/*.js')
+        .pipe(jshint()).pipe(jshint().reporter('default')).pipe(jshint.reporter('fail'));
 });
 
-gulp.task('htmllint',function(){
+gulp.task('htmllint', function () {
     return gulp.src(['travis-stats/*.html'])
         .pipe(htmlhint()).pipe(htmlhint.reporter()).pipe(htmlhint.failReporter())
 });
 
-gulp.task('csslint',function(){
-   return gulp.src('travis-stat/*.css')
-       .pipe(csslint()).pipe(csslint.reporter()).pipe(csslint.failReporter());
+gulp.task('csslint', function () {
+    return gulp.src('travis-stat/*.css')
+        .pipe(csslint()).pipe(csslint.reporter()).pipe(csslint.failReporter());
 });
