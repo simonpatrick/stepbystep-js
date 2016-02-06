e733360a44ad750842064ccf8c7270ca05a153b3
simonpatrick <simonpatrick@163.com>
2016-02-06 10:51:05 +0800

    update js template

--- css_samples/admin_board_samples/api_worksapce/js/apiWorkspace.js
+++ css_samples/admin_board_samples/api_worksapce/js/apiWorkspace.js
@@ -2,3 +2,11 @@
  * Created by patrick on 16/2/5.
  */
 
+var apiWorkspace = angular.module('apiWorkspace', ['ng-admin']);
+apiWorkspace.config(['NgAdminConfigurationProvider'],
+    function (NgAdminConfigurationProvider) {
+        var nga = NgAdminConfigurationProvider;
+        var admin = nga.application('apiWorkspace');
+        //place menu page configuration here
+        nga.configure(admin);
+    });
